e13e329f7ad6b8362e83dd13839fa6ab90095597
Igor Karpinskiy <ik@altimho.ru>
2016-07-08 14:21:11 +0300

    Rebuilded dist.

--- dist/apischeme.js
+++ dist/apischeme.js
@@ -10,14 +10,16 @@
         }
         Provider.prototype.register = function (scheme, processor) {
             var whitelist = this.$sceDelegateProvider.resourceUrlWhitelist();
-            this.schemes[scheme] = function (url) { return url.replace(scheme + ":/", processor); };
+            if (angular.isFunction(processor)) {
+                this.schemes[scheme] = processor;
+            }
+            else {
+                this.schemes[scheme] = function (url) { return url.replace(scheme + ":/", processor); };
+            }
             this.$sceDelegateProvider.resourceUrlWhitelist(whitelist.concat([(scheme + "://**")]));
         };
         Provider.prototype.process = function (scheme, url) {
-            return !!this.schemes[scheme] && this.schemes[scheme](url) || url;
-        };
-        Provider.prototype.exists = function (scheme) {
-            return !!this.schemes[scheme];
+            return (this.schemes[scheme] || angular.identity)(url);
         };
         Provider.prototype.$get = function () {
             return {
@@ -25,12 +27,9 @@
             };
         };
         Provider.prototype.request = function (config) {
-            var scheme = SCHEME_EXPR.exec(config.url);
-            if (!!scheme &&
-                !!scheme[1] &&
-                this.exists(scheme[1]) &&
-                config.url.indexOf(scheme[1] + "://") === 0) {
-                config.url = this.process(scheme[1], config.url);
+            var _a = SCHEME_EXPR.exec(config.url) || [], scheme = _a[1];
+            if (angular.isDefined(this.schemes[scheme])) {
+                config.url = this.process(scheme, config.url);
             }
             return config;
         };
